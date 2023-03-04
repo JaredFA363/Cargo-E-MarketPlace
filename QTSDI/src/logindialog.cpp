bd7c92f7ea7b443012598ab50d95d06bca5d35f3
jared <N0992216@my.ntu.ac.uk>
2023-03-04 08:41:34 -0800

    IMplemented login system

--- QTSDI/src/logindialog.cpp
+++ QTSDI/src/logindialog.cpp
@@ -31,11 +31,12 @@ void LoginDialog::on_Login_pushButton_clicked()
     QString acc_type = ui->Login_comboBox->currentText();
     QString username = ui->Login_username->text();
     QString password = ui->Login_password->text();
+    QString username_1 = "'"+username+"'";
+    QString db_pass;
     QSqlQuery query;
 
     if (acc_type == "Transportation Company")
     {
-        QString username_1 = "'"+username+"'";
         if(query.exec("SELECT password FROM transportcompany WHERE username ="+username_1+""))
         {
             query.first();
@@ -56,10 +57,42 @@ void LoginDialog::on_Login_pushButton_clicked()
     }
     else if (acc_type == "Driver")
     {
-
+        if(query.exec("SELECT password FROM drivers WHERE username ="+username_1+""))
+        {
+            query.first();
+            QString db_pass = query.value(0).toString();
+            if (db_pass == password)
+            {
+                QMessageBox::information(this,"Login","Success");
+            }
+            else
+            {
+                QMessageBox::information(this,"Login","Incorrect Password");
+            }
+        }
+        else
+        {
+            QMessageBox::information(this,"Login","Incorrect Username");
+        }
     }
     else if (acc_type == "Cargo Company")
     {
-
+        if(query.exec("SELECT password FROM cargoowner WHERE username ="+username_1+""))
+        {
+            query.first();
+            QString db_pass = query.value(0).toString();
+            if (db_pass == password)
+            {
+                QMessageBox::information(this,"Login","Success");
+            }
+            else
+            {
+                QMessageBox::information(this,"Login","Incorrect Password");
+            }
+        }
+        else
+        {
+            QMessageBox::information(this,"Login","Incorrect Username");
+        }
     }
 }
